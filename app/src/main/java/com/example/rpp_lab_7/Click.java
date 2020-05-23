a94536faa4d989d02de816c82765ef1fd937de41
Alex <shurik.pankova@gmail.com>
2020-05-23 15:57:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rpp_lab_7/Click.java
@@ -0,0 +1,5 @@
+package com.example.rpp_lab_7;
+
+public interface Click {
+    public void click(int position);
+}
---
+++ app/src/main/java/com/example/rpp_lab_7/DataChangedListener.java
@@ -0,0 +1,5 @@
+package com.example.rpp_lab_7;
+
+public interface DataChangedListener {
+    public void notifyDataChanged();
+}
